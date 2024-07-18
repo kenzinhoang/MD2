a4f5d3a8fbebe317dd06c2179cf7928e02eae965
DatHoang <8732206@gmail.com>
2024-07-18 20:36:52 +0900

    update

---
+++ Session04/baitap/src/Circle.java
@@ -0,0 +1,41 @@
+//public class Circle {
+//    private float banKinh;
+//    private int mauSac;
+//    private float c;
+//    private float s;
+//
+//    public final float pi = 3.14f;
+//
+//    public Circle(float banKinh) {
+//        this.banKinh = banKinh;
+//    }
+//
+//    public Circle() {
+//
+//    }
+//
+//    public float getBanKinh() {
+//        return banKinh;
+//    }
+//
+//    public void setBanKinh(int banKinh) {
+//
+//    }
+//
+//    public float tinhC() {
+//        return c = 2 * pi * banKinh;
+//
+//    }
+//
+//    public float tinhS() {
+//        return s = pi * banKinh;
+//    }
+//
+//    public int inputData(int banKinhHinhTron) {
+//
+//    }
+//
+//    ;
+//
+//
+//}

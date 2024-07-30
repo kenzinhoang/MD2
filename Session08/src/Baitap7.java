d5332fe49fcc65d020896c6778dcb4a496687726
DatHoang <8732206@gmail.com>
2024-07-30 15:33:26 +0900

    update

--- Session08/src/Baitap7.java
+++ Session08/src/Baitap7.java
@@ -1,21 +1,29 @@
 public class Baitap7 {
     public static void main(String[] args) {
         String myList = "21abc";
-        int numberList = 0;
-        String a = "";
-        try {
-            for (int i = 0; i < myList.length(); i++) {
-                System.out.println(myList.charAt(i));
-//                a = String.valueOf(myList.charAt(i));
-//
-//                System.out.println(a + "check a");
-                //numberList += Integer.valueOf(myList.charAt(i));
+        String resultStr = convert(myList);
+        System.out.println("str trước khi đổi: " + myList);
+        System.out.println("str sau khi đổi: " + resultStr);
+    }
 
+    public static String convert(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char i : input.toCharArray()) {
+            try {
+                if (Character.isDigit(i)) {
+                    sb.append(i);
+
+                } else {
+                    throw new NumberFormatException("Ký tự không hợp lệ: " + i);
+                }
+            } catch (NumberFormatException e) {
+                sb.append('0');
             }
-        } catch (NumberFormatException e) {
-            // numberList = numberList.charAt(i)
-            System.out.println(e.getMessage());
         }
 
+        return sb.toString();
     }
+
+
 }
